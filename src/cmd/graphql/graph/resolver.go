a0f687943cb3280ea499e44c9938da813ffef8d5
cindy1408 <77013299+cindy1408@users.noreply.github.com>
2022-04-07 23:03:52 +0100

    generate schema and create base exercise

---
+++ src/cmd/graphql/graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/cindy1408/gym/src/cmd/graphql/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	baseExercise []*model.BaseExercise
+}
---
+++ src/cmd/graphql/graph/schema.resolvers.go
@@ -0,0 +1,63 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/cindy1408/gym/src/cmd/graphql/graph/generated"
+	"github.com/cindy1408/gym/src/cmd/graphql/graph/model"
+)
+
+func (r *mutationResolver) CreateBaseExercise(ctx context.Context, input *model.BaseExerciseInput) (string, error) {
+	muscleGroup := []*model.MuscleGroup{}
+	muscleGroup = append(muscleGroup, &model.MuscleGroup{input.MuscleGroup})
+
+	newExercise := &model.BaseExercise{
+		ID:            "",
+		Name:          input.Name,
+		MuscleGroup:   muscleGroup,
+		SpecificParts: []*model.Body{{input.SpecificParts}},
+		Level:         input.Level,
+		AvoidGiven:    []*model.AvoidGiven{{input.AvoidGiven}},
+		MovementType:  input.MovementType,
+	}
+	r.baseExercise = append(r.baseExercise, newExercise)
+
+	return "exercise was successfully added", nil
+}
+
+func (r *queryResolver) BaseExercises(ctx context.Context) ([]*model.BaseExercise, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) GetAllAvaliableBaseExercises(ctx context.Context) ([]string, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) GetAllEachExercise(ctx context.Context) ([]*model.EachExercise, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) GetAllWorkoutDay(ctx context.Context) ([]*model.WorkoutPerDay, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) GetAllUserWorkoutPlans(ctx context.Context) ([]*model.UserWorkoutPlan, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) GetMuscleSpecifics(ctx context.Context, input *model.MuscleSpecificInput) (string, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
